ffaf1e67fccb73aea263f729fa6a3a8b0e35fdbe
Byakko12 <davidlandg@gmail.com>
2022-05-31 21:13:38 -0500

    files fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """app.py to connect to API"""
-import os
+from os import getenv
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, jsonify, make_response
 
+HBNB_API_HOST = getenv('HBNB_API_HOST')
+HBNB_API_PORT = getenv('HBNB_API_PORT')
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -16,7 +18,21 @@ def teardown_appcontext(code):
     storage.close()
 
 
+@app.errorhandler(404)
+def error_404(self):
+    """ handle error_404 """
+    error_dict = {"error": "Not found"}
+    return make_response(jsonify(error_dict), 404)
+
+
+def port_host(HBNB_API_HOST, HBNB_API_PORT):
+    """set up host and port variables"""
+    if not HBNB_API_HOST:
+        HBNB_API_HOST = '0.0.0.0'
+    if not HBNB_API_PORT:
+        HBNB_API_PORT = '5000'
+
+
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
-    threaded = True
+    port_host(HBNB_API_HOST, HBNB_API_PORT)
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, debug=True, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
 """index.py to connect to API"""
-from api.v1.views import app_views, jsonify
+from api.v1.views import app_views
+from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 classes = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User
 }
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def status():
     """Status"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
+@app_views.route('/stats', methods=['GET'])
 def Stats():
     """Stats"""
     dict = {}
     for key, value in classes.items():
         dict[key] = storage.count(value)
     return jsonify(dict)
-
-if __name__ == "__main__":
-    pass
