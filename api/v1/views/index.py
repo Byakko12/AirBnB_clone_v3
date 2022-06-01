230f7a1bc8fdb1bb7bdda1046dbc316d0f185c33
Byakko12 <davidlandg@gmail.com>
2022-05-31 20:10:19 -0500

    add file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
 """index.py to connect to API"""
 from api.v1.views import app_views, jsonify
+from models import storage
+classes = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    """hbnbStatus"""
+    """Status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def Stats():
+    """Stats"""
+    dict = {}
+    for key, value in classes.items():
+        dict[key] = storage.count(value)
+    return jsonify(dict)
